d64c57c89e3825f5aa34d3e1d447ab70b9ba16ec
Adam Stewart <adamjoesph11@gmail.com>
2022-12-01 21:32:29 -0600

    adding content and InTune Screenshot

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const pages = ["index.html","resume.html","background.html","contact.html","truss.html"]
 
 function onKeyUp (e){
-    console.log(e)
-    console.log(e.srcElement.tabIndex)
     index = e.srcElement.tabIndex
-    console.log(e.target.nodeName)
     eleType = e.target.nodeName
     if (e.key === "Enter" && isValidTarget(e)) window.location.assign(pages[index])
 }
 
 function isValidTarget (e){
-    //console.log(e.target.srcElement.class)
-    console.log(e.target.className)
-
+    //console.log(e.target.srcElement)
     return e.target.nodeName === "LI" || e.target.className === "project-card" 
 }
